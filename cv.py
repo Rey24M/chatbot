476abe3d54402667f280c830adfba5a5dbae1d86
Rey <70631863+Rey24M@users.noreply.github.com>
2025-06-10 23:11:06 +0530

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+# Load dataset
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+# Normalize pixel values
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# Add channel dimension for CNN
+train_images = train_images[..., np.newaxis]
+test_images = test_images[..., np.newaxis]
+
+#CNN
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5, validation_split=0.1)
+
+# Evaluate on test set
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
+
+# Predict classes
+pred_labels = np.argmax(model.predict(test_images), axis=1)
+
+# Confusion matrix
+cm = confusion_matrix(test_labels, pred_labels)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[
+    "T-shirt/top","Trouser","Pullover","Dress","Coat",
+    "Sandal","Shirt","Sneaker","Bag","Ankle boot"
+])
+disp.plot(xticks_rotation='vertical')
+plt.show()
+
+# Save model
+model.save('fashion_mnist_cnn.h5')
+
+# Load model (later, without retraining)
+loaded_model = tf.keras.models.load_model('fashion_mnist_cnn.h5')
